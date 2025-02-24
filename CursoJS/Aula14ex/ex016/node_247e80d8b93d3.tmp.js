27fdd0757a235c478f831247375939276907bbc4
Adriano Cristóvao de Mendonça <adriano.cristmend@gmail.com>
2025-02-24 19:57:39 -0300

    aaa

---
+++ CursoJS/Aula14ex/ex016/node_247e80d8b93d3.tmp.js
@@ -0,0 +1,15 @@
+/* function contar() {
+    var inicio = document.getElementById('txtinicio').value
+    var fim = document.getElementById('txtfim').value
+    var passo = document.getElementById('txtpasso').value
+    var c = inicio
+
+    for(c; c <= fim; c++) {
+        res.innerHTML += `Valor ${c} - `
+        c += passo
+    }
+} */
+
+    for (var c = 1; c <= 10; c+2) {
+        console.log(c)
+    }
