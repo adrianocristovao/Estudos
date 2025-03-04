e1800ccfa9e8e763fdfe395a5856e98834e400a2
Adriano Cristóvao de Mendonça <adriano.cristmend@gmail.com>
2025-03-04 13:03:49 -0300

    aula 16

--- CursoJS/Aula15/ambiente.js
+++ CursoJS/Aula15/ambiente.js
@@ -10,7 +10,7 @@ console.log(`O vetor tem ${num.length} posições`)
 
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(88)
+let pos = num.indexOf(8)
 
 if (pos == -1) {
     console.log('O valor não foi encontrado')
